1027e6b35f773997e039b0b68ee26fecf395b866
Richard T Swierk <richard.swierk@mymail.champlain.edu>
2020-04-04 13:38:58 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get install python --yes
+apt-get install python-scapy --yes
+apt-get install python-psutil --yes
+apt-get install python-os --yes
+apt-get install python-random --yes
+apt-get install python-threading --yes
+apt-get install python-time --yes
